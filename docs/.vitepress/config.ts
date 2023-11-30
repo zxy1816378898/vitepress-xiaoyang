aab9c65c0036b63dff15d2ae8493f3d7d9adbcbd
肖恩 <1816378898@qq.com>
2023-11-30 15:19:41 +0800

    1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,55 +1,48 @@
-import { defineConfig } from 'vitepress';
-import { sidebar, nav } from './relaConf';
+import { defineConfig } from "vitepress";
+import { sidebar, nav } from "./relaConf";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/vitepress-xiaoyang/',
-  title: 'XiaoYang\'s Blog',
-  description: 'A VitePress Site',
+    base: "/vitepress-xiaoyang/",
+    title: "XiaoYang's Blog",
+    description: "A VitePress Site",
 
-  // 配置markdown写作风格
-  // markdown: {
-  //   toc: {
-  //     level: [1, 2, 3, 4]
-  //   },
-  //   headers: {
-  //     level: [1, 2, 3, 4]
-  //   },
-  //   // https://github.com/valeriangalliat/markdown-it-anchor#usage
-  //   anchor: {
-  //     // permalink: anchor.permalink.headerLink()
-  //   },
-  //   lineNumbers: true // 让代码块中实现行号
+    // 配置markdown写作风格
+    // markdown: {
+    //   toc: {
+    //     level: [1, 2, 3, 4]
+    //   },
+    //   headers: {
+    //     level: [1, 2, 3, 4]
+    //   },
+    //   // https://github.com/valeriangalliat/markdown-it-anchor#usage
+    //   anchor: {
+    //     // permalink: anchor.permalink.headerLink()
+    //   },
+    //   lineNumbers: true // 让代码块中实现行号
 
-  //   // config: (md) => {
-  //   //   md.use(demoBlockPlugin, {
-  //   //     cssPreprocessor: 'less'
-  //   //   });
-  //   // }
-  // },
+    //   // config: (md) => {
+    //   //   md.use(demoBlockPlugin, {
+    //   //     cssPreprocessor: 'less'
+    //   //   });
+    //   // }
+    // },
 
-  themeConfig: {
-    logo: '/head_portrait.png',
-    nav: nav,
-    sidebar: sidebar,
-    search: {
-      provider: 'local'
+    themeConfig: {
+        logo: "/head_portrait.png",
+        nav: nav,
+        sidebar: sidebar,
+
+        outline: {
+            level: [2, 6],
+            label: "目录",
+        },
+
+        i18nRouting: true,
+
+        socialLinks: [
+            { icon: "github", link: "https://github.com/vuejs/vitepress" },
+        ],
+       
     },
-
-    outline: {
-      level: [2, 6],
-      label: '目录'
-    },
-
-    i18nRouting: true,
-
-    carbonAds: {
-      code: 'your-carbon-code',
-      placement: 'your-carbon-placement'
-    },
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
 });
